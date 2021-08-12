c9bd5f8cc99325ef7760efc25216d800622cf3ec
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2021-08-12 17:07:53 -0500

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,26 +1,8 @@
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 8,
-    center: { lat: -34.397, lng: 150.644 },
-  });
-  const geocoder = new google.maps.Geocoder();
-  document.getElementById("submit").addEventListener("click", () => {
-    geocodeAddress(geocoder, map);
-  });
-}
+document.getElementById("submit").addEventListener("click", () => {
+    const address = document.getElementById("address").value;
+    console.log('click')
+});
 
-function geocodeAddress(geocoder, resultsMap) {
-  const address = document.getElementById("address").value;
-  geocoder
-    .geocode({ address: address })
-    .then(({ results }) => {
-      resultsMap.setCenter(results[0].geometry.location);
-      new google.maps.Marker({
-        map: resultsMap,
-        position: results[0].geometry.location,
-      });
-    })
-    .catch((e) =>
-      alert("Geocode was not successful for the following reason: " + e)
-    );
+function customProcess(input, options) {
+  console.log('ok')
 }
