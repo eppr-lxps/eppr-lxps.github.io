42cfb96bbc92d6634c1b56b30504fd16794faad5
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2021-08-12 18:35:07 -0500

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,6 +1,8 @@
 function readyFn( ) {
     // Code to run when the document is ready.
+    const entrada = document.getElementById("input").value;
     document.getElementById("submit").addEventListener("click", () => {
         console.log('Hello World')
+        console.log(entrada)
     });
 }
