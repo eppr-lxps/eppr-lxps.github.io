605b8d3dc5d4b3490921da453b369365e7bfab4b
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2021-08-13 14:16:41 -0500

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,14 +1,20 @@
 function readyFn( ) {
-    epprSignature()
     // Este código es lanzado por el elemento <body onload="readyFn()">
+    epprSignature()
+    // Definimos 2 constantes al seleccionar elementos por Id.
     const entrada  = document.getElementById("input");
     const selector = document.getElementById("select");
-    // Creamos un "Listener" para escuchar eventos en nuestra página, en este ejemplo, el evento es "click".
+
+    // Creamos un "Listener" para escuchar eventos en el elemento selector en este ejemplo, el evento es "change".
     selector.addEventListener("change", (event) => {
+        // Imprimir en la consola de nuestro navegador el valor del elemento seleccionado. 
         console.log(event.target.value)
     })
+    
+    // Creamos un "Listener" para escuchar eventos en nuestra página, en este ejemplo, el evento es "click".
     document.getElementById("submit").addEventListener("click", () => {
         // Aquí es donde vamos a realizar todas nuestras actividades.
+        var algoritmo = selector.value // Actualizar el valor más reciente de la constante "selector".
         var texto = entrada.value // Actualizar el valor de la constante "entrada", por si ha cambiado recientemente.
         texto = texto.toString() // Este paso convierte cualquier valor en una cadena de texto (String variable)
         // ACTIVIDADES (Algoritmos):
@@ -19,16 +25,26 @@ function readyFn( ) {
         // 5) Determinar si un número es par o impar. // Operador Resto (x % 2)
         myFunction(texto)
     });
+    
 }
+
+// Función predeterminada que nos ayuda a imprimir en consola y además a modificar el resultado.
 function myFunction( texto ) {
+    // Seleccionar Elemento con clase "resultado"
+    const resultado = document.querySelector('.resultado');
+    // Modificar el contenido del elemento seleccionado.
+    resultado.textContent = `${texto}`;
+    // Imprimir en la consola de nuestro navegador.
     console.log(texto)
 }
+
 // Ejemplo de una función nueva
 function newFunction( input ) {
     // El nombre de esta funcion es "newFunction" y tiene una variable de entrada llamada "input"
     // Ejemplo para usar esta función: newFunction("Hola")
 }
 
+// Función para imprimir el logotipo de EPPR en la consola al cargar la página.
 function epprSignature(){
     console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
     console.log("%%%%%%%%%%%%%%%/ .*/#%%%%%%%%%%%%%%%%%%%%")
@@ -51,4 +67,5 @@ function epprSignature(){
     console.log("%%%%%%%%%%%%%%%%%%%#/.     .,#%%%%%%%%%%%")
     console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
     console.log("%%%%%%%%%%% EPPR CLASE 304 %%%%%%%%%%%%%%")
+    console.log("Visita https://github.com/eppr/304 Para más información.")
 }
