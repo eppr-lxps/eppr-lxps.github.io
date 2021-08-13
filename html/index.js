40339cd503bc8ce32b5fdc829c09598dd9e100d2
EPPR LXPS <67383945+eppr-lxps@users.noreply.github.com>
2021-08-12 19:02:33 -0500

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,8 +1,25 @@
 function readyFn( ) {
-    // Code to run when the document is ready.
+    // Este código es lanzado por el elemento <body onload="readyFn()">
     const entrada = document.getElementById("input");
+    // Creamos un "Listener" para escuchar eventos en nuestra página, en este ejemplo, el evento es "click".
     document.getElementById("submit").addEventListener("click", () => {
-        console.log('Hello World')
-        console.log(entrada.value)
+        // Aquí es donde vamos a realizar todas nuestras actividades.
+        var texto = entrada.value // Actualizar el valor de la constante "entrada", por si ha cambiado recientemente.
+        texto = texto.toString() // Este paso convierte cualquier valor en una cadena de texto (String variable)
+        // ACTIVIDADES (Algoritmos):
+        // 1) Todo el texto en Mayúsculas. // .toUpperCase() 
+        // 2) Todo el texto en Minúsculas. // .toLowerCase()
+        // 3) Detectar si el texto es un número o no. // .isNaN()
+        // 4) Solo aceptar números. // .replace(/\D/g,'') // RegEx \D 
+        // 5) Determinar si un número es par o impar. // Operador Resto (x % 2)
+        myFunction(texto)
     });
+}
+function myFunction( input ) {
+    console.log(texto)
+}
+// Ejemplo de una función nueva
+function newFunction( input ) {
+    // El nombre de esta funcion es "newFunction" y tiene una variable de entrada llamada "input"
+    // Ejemplo para usar esta función: newFunction("Hola")
 }
